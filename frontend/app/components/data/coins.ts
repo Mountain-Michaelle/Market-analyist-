96c91d01e49d47a33c1199afa54ce80ce89b53b6
unknown <michaelchinemelu24@gmail.com>
2025-10-01 05:15:40 +0100

    added more string of coins

--- frontend/app/components/data/coins.ts
+++ frontend/app/components/data/coins.ts
@@ -1,4 +1,23 @@
+
 export const coins = [
-    'bitcoin',
-    'ethereum',
-]
+  "bitcoin",
+  "ethereum",
+  "tether",
+  "binancecoin",
+  "usd-coin",
+  "ripple",
+  "cardano",
+  "dogecoin",
+  "polkadot",
+  "binance-usd",
+  "uniswap",
+  "litecoin",
+  "bitcoin-cash",
+  "chainlink",
+  "stellar",
+  "vechain",
+  "solana",
+  "wrapped-bitcoin",
+  "ethereum-classic",
+  "monero"
+];

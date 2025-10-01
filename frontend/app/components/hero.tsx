e488dde3c7a9d3319603df80bc49452af4136bdd
unknown <michaelchinemelu24@gmail.com>
2025-10-01 04:46:58 +0100

    updated the hero ui again

--- frontend/app/components/hero.tsx
+++ frontend/app/components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
    <div className="flex justify-center items-center mt-7 px-4 md:mb-20">
   <div className="text-center max-w-[90%] md:max-w-[70%]">
-    <h1 className="text-5xl text-center sm:text-6xl font-extrabold animate-pulse duration-1000 md:text-5xl lg:text-7xl bg-gradient-to-r p-2 from-black to-blue-600 bg-clip-text text-transparent">
+    <h1 className="text-4xl text-center sm:text-6xl font-extrabold animate-pulse duration-1000 md:text-5xl lg:text-7xl bg-gradient-to-r p-2 from-black to-blue-600 bg-clip-text text-transparent">
       Know. Analyze. Decide.
     </h1>
     <p className="mt-3 text-lg sm:text-base text-blue-900 font-bold md:text-lg lg:text-xl">
